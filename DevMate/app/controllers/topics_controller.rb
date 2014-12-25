3880bc73a3376d8f603dc5e424e842d255ebbdb5
hankyu-yatsu <ssymsh28+Git@gmail.com>
2014-12-25 09:49:38 +0900

    資料の新規アップロード機能を実装

--- DevMate/app/controllers/topics_controller.rb
+++ DevMate/app/controllers/topics_controller.rb
@@ -1,2 +1,23 @@
 class TopicsController < ApplicationController
+  def new
+    @topic = Topic.new
+    @devpro = Devpro.where(params[:devpro_id]).first
+  end
+  
+  def create
+    topic = Topic.create(devpro_id: params[:topic][:devpro_id], name: params[:topic][:name])
+    material = Material.create(topic_id: topic.id, summary: params[:material][:summary])
+    for content_num in 1..5
+      content_index_params = params[:content]["#{content_num}"]
+      if content_index_params.nil?
+        break
+      end
+      file = content_index_params[:content]
+      original_filename = file.original_filename
+      created_filename = Time.now.strftime("%y%m%d_%H%M%S_#{original_filename}")
+      File.open("uploads/contents/#{created_filename}", "wb"){|f| f.write(file.read)}
+      Content.create(material_id: material.id, content:"/uploads/contents/#{created_filename}")
+    end
+    redirect_to material_path(material.id) 
+  end
 end
